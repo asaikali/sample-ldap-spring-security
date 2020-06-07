bb97a523a27eb4821b7cad34626019a65606bf28
Adib Saikali <asaikali@gmail.com>
2020-06-07 18:08:21 -0400

    Add test LDAP Server

---
+++ ldap-server/src/main/java/com/example/ldapserver/RootController.java
@@ -0,0 +1,18 @@
+package com.example.ldapserver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class RootController {
+
+  @Value("${spring.ldap.embedded.port}")
+  private String port;
+
+  @GetMapping("/")
+  String  get() {
+    return "Embedded LDAP server running on port  " + port;
+  }
+}
