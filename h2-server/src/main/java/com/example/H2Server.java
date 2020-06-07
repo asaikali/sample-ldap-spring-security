4ef17aff2b73b890f277c6980df46ae9a21917f0
Adib Saikali <asaikali@gmail.com>
2020-06-07 19:16:49 -0400

    Add h2 database server

---
+++ h2-server/src/main/java/com/example/H2Server.java
@@ -0,0 +1,11 @@
+package com.example;
+
+
+import java.sql.SQLException;
+import org.h2.tools.Console;
+
+public class H2Server {
+  public static void main(String[] args) throws SQLException {
+    Console.main( "-webPort", "6565", "-tcpPort", "9096");
+  }
+}
